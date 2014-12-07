b312c77624bd4bbd351333306368a6e5e65e29d9
wkennedy3125 <wkennedy3125@gmail.com>
2014-12-07 12:08:02 -0600

    update

--- Plot1.R
+++ Plot1.R
@@ -8,6 +8,10 @@
 #    Histogram of 'Global Active Power'
 #    from 2007-02-01 to 2007-02-02
 ##############################################################################
+# Set working directory
+#setwd("/Users/adakemia/Documents/Academic/Coursera/DataScienceSpecialization/04ExploratoryDataAnalysis/Projects/Project1/ExData_Plotting1")
+#list.files("../")
+##############################################################################
 # Package dependencies
 ##############################################################################
 library(dplyr) # for filter()
@@ -24,17 +28,14 @@ source("./FileHandlingFunctions.R")
 file1 <- "household_power_consumption.txt"
 # Data directory: where the files / data are located relative to 
 # working directory (i.e., the data subdirectory)
-dataDir <- "data"
+fileDir <- "data"
+# Check if file exists in the working directory
+IsThisFileAvailable(file1, fileDir)  #Just playing around with potential
+                                     #helper functions
+#IsThisFileAvailable(file1)
 ##############################################################################
 # MAIN 
 ##############################################################################
-# Set working directory
-#setwd("/Users/adakemia/Documents/Academic/Coursera/DataScienceSpecialization/04ExploratoryDataAnalysis/Projects/Project1/ExData_Plotting1")
-#list.files("../")
-
-# Check if file exists in the working directory
-#-------------------------
-IsThisFileAvailable(file1, fileDir)
 
 # Read data
 #-------------------------
@@ -42,19 +43,20 @@ IsThisFileAvailable(file1, fileDir)
 # Read in full data set (after confirming size and requirements)
 # NOTE: may need to reduce prior to reading in using grep,sed,awk,etc
 #       In this case, on my current machine, not needed
-data_full <- fread("./data/household_power_consumption.txt", na.strings="?",
-            stringsAsFactors=FALSE)
-#dim(data_full)
+# data_full <- fread("./data/household_power_consumption.txt", na.strings="?",
+#             stringsAsFactors=FALSE)
+# dim(data_full)
 # Subset data by Date using dplyr
-data <- filter(data_full, grep("^[1,2]/2/2007", Date))
-#dim(data)
+# data <- filter(data_full, grep("^[1,2]/2/2007", Date))
+# dim(data)
 # OR subset using data.table (better probably)
 # (will add this later)
 
 # OR subset prior to importing (best but need to get first line so needs work)
-#system("egrep '^household|^[1,2]/2/2007' ./data/household_power_consumption.txt > ./data/household_power_consumption_subset.txt")
-#data <- fread("./data/household_power_consumption_subset.txt", na.strings="?",
-#                   stringsAsFactors=FALSE, header=TRUE)
+system("head -1 ./data/household_power_consumption.txt > ./data/household_power_consumption_subset.txt")
+system("grep '^[1,2]/2/2007' ./data/household_power_consumption.txt >> ./data/household_power_consumption_subset.txt")
+data <- fread("./data/household_power_consumption_subset.txt", na.strings="?",
+                   stringsAsFactors=FALSE, header=TRUE)
 #dim(data)
 #str(data)
 # Data cleaning
